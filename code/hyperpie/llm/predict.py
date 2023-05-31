73b707d36ba5e75be1e1ed6d911fc3b66546f501
Tarek Saier <tareksaier@gmail.com>
2023-05-31 17:05:42 +0200

    add demo script for hyperpie package

--- code/hyperpie/llm/predict.py
+++ code/hyperpie/llm/predict.py
@@ -1,8 +1,9 @@
 import datetime
 import json
+import openai
 
 
-def prompt_and_save(para, prompt, params, o_ai):
+def prompt_and_save(para, prompt, params=None, verbose=False):
     """ Convenience method that
         - gets completion from OpenAI API
         - saves result to a JSON file
@@ -11,10 +12,13 @@ def prompt_and_save(para, prompt, params, o_ai):
         - returns the completion
     """
 
+    if params is None:
+        hyperpie.llm.default_params
+
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     out_fn = f"prompt_result_{timestamp}.json"
 
-    completion = o_ai.Completion.create(prompt=prompt, **params)
+    completion = openai.Completion.create(prompt=prompt, **params)
     out_dict = {
         "paragraph": para,
         "timestamp": timestamp,
@@ -26,11 +30,12 @@ def prompt_and_save(para, prompt, params, o_ai):
     with open(out_fn, "w") as f:
         json.dump(out_dict, f, indent=4)
 
-    print('- - - Prompt - - -')
-    print(prompt)
-    print('- - - Completion - - -')
-    print(out_dict["completion"]["choices"][0]["text"])
-    print('- - - Saved to - - -')
-    print(out_fn)
+    if verbose:
+        print('- - - Prompt - - -')
+        print(prompt)
+        print('- - - Completion - - -')
+        print(out_dict["completion"]["choices"][0]["text"])
+        print('- - - Saved to - - -')
+        print(out_fn)
 
     return out_dict
