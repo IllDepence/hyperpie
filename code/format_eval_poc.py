f821b74f19e68d1e7020864b86cd4686c4be830b
Tarek Saier <tareksaier@gmail.com>
2023-07-20 08:55:56 +0200

    minor refactoring of LLM format stats aggregation

--- code/format_eval_poc.py
+++ code/format_eval_poc.py
@@ -3,63 +3,24 @@
 
 import json
 import hyperpie as hp
-from collections import defaultdict
 
-# load data from completion cache
+
+# load GALACTICA data from completion cache
 with open('hyperpie/llm/completion_cache.json') as f:
     cc = json.load(f)
-
 cc_dicts = [cd for cd in cc['4207bd25b65a13f7663c1cea7a8073df'].values()]
 
-# aggregate stats
-aggregate_data = {
-    'preprocessor': {
-        'no_yaml_found': 0, 'empty_yaml': 0, 'garbage_around_yaml': 0
-    },
-    'yaml2json': {'parse_fail': 0, 'parsing_error_dict': defaultdict(list)},
-    'coarse_structure': {'coarse_structure_error': 0},
-    'json_content': {
-        'num_ents_intext_notintext': [0, 0],
-        'num_ent_types_valid_invalid': [0, 0],
-        'num_aids_valid_invalid': [0, 0],
-        'num_pids_valid_invalid': [0, 0],
-        'num_vids_valid_invalid': [0, 0],
-        'num_cids_valid_invalid': [0, 0]
-    }
-}
+stats_dicts = []
 
 for cc_dict in cc_dicts:
-    annot_j, data = hp.llm.convert.llm_output2eval_input(
+    annot_j, stats_dict = hp.llm.convert.llm_output2eval_input(
         cc_dict,
         llm_annotated_text='foo',
         matched_surface_forms=True,
         preprocessor=hp.llm.convert.galactica_yaml_extract
     )
-    # Aggregate 'preprocessor' data
-    if 'preprocessor' in data:
-        for stat_name, value in data['preprocessor'].items():
-            if value is not None:
-                aggregate_data['preprocessor'][stat_name] += value
+    stats_dicts.append(stats_dict)
 
-    # Aggregate 'yaml2json' data
-    if 'parse_fail' in data['yaml2json'] and data['yaml2json']['parse_fail']:
-        aggregate_data['yaml2json']['parse_fail'] += 1
-    if 'parsing_error_dict' in data['yaml2json']:
-        for error_type, msg in data['yaml2json']['parsing_error_dict'].items():
-            aggregate_data['yaml2json']['parsing_error_dict'][
-                error_type
-            ].append(msg)
+aggregate_stats = hp.llm.convert.aggregate_format_stats(stats_dicts)
 
-    if 'coarse_structure' in data:
-        if data['coarse_structure']['coarse_structure_error']:
-            aggregate_data['coarse_structure']['coarse_structure_error'] += 1
-
-    if 'json_content' in data:
-        # Aggregate 'json_content' data
-        for stat_name, values in data['json_content'].items():
-            aggregate_data['json_content'][stat_name] = [
-                sum(x) for x in
-                zip(values, aggregate_data['json_content'][stat_name])
-            ]
-
-print(json.dumps(aggregate_data, indent=4))
+print(json.dumps(aggregate_stats, indent=4))
--- code/hyperpie/llm/convert.py
+++ code/hyperpie/llm/convert.py
@@ -13,6 +13,7 @@ import sys
 import json
 import uuid
 import yaml
+from collections import defaultdict
 from difflib import SequenceMatcher
 from hyperpie.util.annot import (
     entity_dict, empty_para_annotation, relation_dict,
@@ -765,6 +766,67 @@ def _preprocessor_status_dict(
         'empty_yaml': empty_yaml,
         'garbage_around_yaml': garbage_around_yaml
     }
+
+
+def aggregate_format_stats(stats_dicts):
+    """ Aggregate a list of stats return by llm_output2eval_input.
+    """
+
+    aggregate_stats = {
+        'num_total': len(stats_dicts),
+        'preprocessor': {
+            'no_yaml_found': 0, 'empty_yaml': 0, 'garbage_around_yaml': 0
+        },
+        'yaml2json': {
+            'parse_fail': 0, 'parsing_error_dict': defaultdict(list)
+        },
+        'coarse_structure': {'coarse_structure_error': 0},
+        'json_content': {
+            'num_ents_intext_notintext': [0, 0],
+            'num_ent_types_valid_invalid': [0, 0],
+            'num_aids_valid_invalid': [0, 0],
+            'num_pids_valid_invalid': [0, 0],
+            'num_vids_valid_invalid': [0, 0],
+            'num_cids_valid_invalid': [0, 0]
+        }
+    }
+
+    for stats_dict in stats_dicts:
+        # Aggregate preprocessor stats
+        if 'preprocessor' in stats_dict:
+            for stat_name, value in stats_dict['preprocessor'].items():
+                if value is not None:
+                    aggregate_stats['preprocessor'][stat_name] += value
+
+        # Aggregate 'yaml2json' stats
+        if (
+            'parse_fail' in stats_dict['yaml2json'] and
+            stats_dict['yaml2json']['parse_fail']
+        ):
+            aggregate_stats['yaml2json']['parse_fail'] += 1
+        if 'parsing_error_dict' in stats_dict['yaml2json']:
+            for error_type, msg in stats_dict['yaml2json'][
+                'parsing_error_dict'
+            ].items():
+                aggregate_stats['yaml2json']['parsing_error_dict'][
+                    error_type
+                ].append(msg)
+
+        if 'coarse_structure' in stats_dict:
+            if stats_dict['coarse_structure']['coarse_structure_error']:
+                aggregate_stats['coarse_structure'][
+                    'coarse_structure_error'
+                ] += 1
+
+        if 'json_content' in stats_dict:
+            # Aggregate 'json_content' stats
+            for stat_name, values in stats_dict['json_content'].items():
+                aggregate_stats['json_content'][stat_name] = [
+                    sum(x) for x in
+                    zip(values, aggregate_stats['json_content'][stat_name])
+                ]
+
+    return aggregate_stats
 
 
 def galactica_yaml_extract(llm_output_dict, verbose=False):
