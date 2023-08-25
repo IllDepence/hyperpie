043eea1172b94eedeb509bb06f208c5ee318ff07
Tarek Saier <tareksaier@gmail.com>
2023-08-25 18:46:41 +0200

    implement unlbld data merged NER+RE pred. printing

--- code/PL-Marker/eval/postprocess_unlabeled_data_prediction.py
+++ code/PL-Marker/eval/postprocess_unlabeled_data_prediction.py
@@ -71,9 +71,64 @@ def merge(root_dir):
     return merged_pred
 
 
+def print_predictions(paras):
+    """ Print merged NER+RE predictions for inspection.
+    """
+
+    # iterate over paras
+    for para in paras:
+        print(f'= = = {para["doc_key"]} = = =')
+        para_delta = 0
+        # iterate over sentences
+        for sent_idx, sent in enumerate(para['sentences']):
+            # print('[' + '] ['.join(sent) + ']')
+            sent_str = ' '.join(sent)
+            print(sent_str)
+            ner_pred = para['predicted_ner'][sent_idx]
+            ner_gold = para['ner'][sent_idx]
+            print('<<<NER>>>')
+            offs_to_label = {}
+            for (start, end, label) in ner_pred:
+                print(sent[start-para_delta:end-para_delta+1], label)
+                offs_to_label[(start, end)] = label
+            # print('<<<NER>>> (gold)')
+            for (start, end, label) in ner_gold:
+                # print(sent[start-para_delta:end-para_delta+1], label)
+                offs_to_label[(start, end)] = label
+            has_rels = False
+            if 'predicted_relations' in para:
+                re_pred = para['predicted_relations'][sent_idx]
+                # data cleaning
+                # re_pred_clean = []
+                # for (
+                #     start_from, end_from, start_to, end_to, label
+                # ) in re_pred:
+                #     label_from = offs_to_label.get((start_from, end_from))
+                #     label_to = offs_to_label.get((start_to, end_to))
+                print('<<<RE>>>')
+                for (
+                    start_from, end_from, start_to, end_to, label
+                ) in re_pred:
+                    has_rels = True
+                    label_from = offs_to_label.get((start_from, end_from))
+                    label_to = offs_to_label.get((start_to, end_to))
+                    print(
+                        sent[start_from-para_delta:end_from-para_delta+1],
+                        '--',
+                        label,
+                        '->',
+                        sent[start_to-para_delta:end_to-para_delta+1],
+                        f'({label_from} -> {label_to})'
+                    )
+            para_delta += len(sent)
+            if has_rels:
+                input()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('root_dir', type=str)
     args = parser.parse_args()
 
-    merge(args.root_dir)
+    paras = merge(args.root_dir)
+    print_predictions(paras)
