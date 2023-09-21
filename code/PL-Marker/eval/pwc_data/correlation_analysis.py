c5b0abc8b6a2254ceb78053ef4ace8b53cc6957f
Tarek Saier <tareksaier@gmail.com>
2023-09-21 10:04:58 +0200

    work on reproducibility correlation analysis

--- code/PL-Marker/eval/pwc_data/correlation_analysis.py
+++ code/PL-Marker/eval/pwc_data/correlation_analysis.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,15 +9,14 @@ from collections import defaultdict
 from scipy.stats import pearsonr
 
 
-def load_pediction_data(root_dir):
+def load_pediction_data(prediction_data_fp):
     """ Get arxiv ids of papers for which we extracted
         hyperparameter info.
     """
 
     pred_pprs = defaultdict(list)
 
-    nerre_pred_fn = 'merged_preds.jsonl'
-    with open(os.path.join(root_dir, nerre_pred_fn), 'r') as f:
+    with open(prediction_data_fp, 'r') as f:
         for line in f.readlines():
             para = json.loads(line)
             doc_key = para['doc_key']
@@ -123,7 +123,13 @@ def _get_ppr_data_digest(ppr_data):
         'repo_forks': -1,
         'repo_archived': -1,
         'repo_open_issues': -1,
-        'repo_has_license': -1
+        'repo_closed_issues': -1,
+        'repo_pulls_count': -1,
+        'repo_commits_count': -1,
+        'repo_contributor_count': -1,
+        'repo_releases_count': -1,
+        'repo_readme_len': -1,
+        'repo_forks_per_star': -1,
     }
     if ppr_data is None:
         return digest
@@ -144,7 +150,17 @@ def _get_ppr_data_digest(ppr_data):
         digest['repo_forks'] = gh_meta['forks_count']
         digest['repo_archived'] = int(gh_meta['archived'])
         digest['repo_open_issues'] = gh_meta['open_issues_count']
-        digest['repo_has_license'] = int(gh_meta['license'] is not None)
+        digest['repo_closed_issues'] = gh_meta['closed_issues_count']
+        digest['repo_pulls_count'] = gh_meta['pulls_count']
+        digest['repo_commits_count'] = gh_meta['commits_count']
+        digest['repo_contributor_count'] = gh_meta['contributors_count']
+        digest['repo_releases_count'] = gh_meta['releases_count']
+        digest['repo_readme_len'] = gh_meta['readme_len']
+        digest['repo_readme_len'] = gh_meta['readme_len']
+        digest['repo_forks_per_star'] = (
+            digest['repo_forks'] / digest['repo_stars']
+            if digest['repo_stars'] else 0
+        )
     return digest
 
 
@@ -156,12 +172,15 @@ def analyse(ppr_preds, ppr_datas):
     # prep lists for selected eval
     # (n_vpa <-> repo_stars / repo_watchers / repo_open_issues)
     n_vpa_vals = []
-    repo_stars_vals = []
+    repo_corr_vals = []
     log_repo_stars_vals = []
     repo_forks_vals = []
     repo_open_issues_vals = []
 
-    star_threshold = 50
+    star_threshold = 0
+    filter_repos = [  # because they are not for a single paper
+        'google-research/google-research',
+    ]
 
     for arxiv_id in ppr_preds.keys():
         # print(f'paper: {arxiv_id}')
@@ -175,37 +194,65 @@ def analyse(ppr_preds, ppr_datas):
             continue
         # print(f'num paras: {len(ppr_pred)}')
         hyper_digest = _get_ppr_pred_hyperparam_digest(ppr_pred)
-        repro_digest = _get_ppr_data_digest(ppr_data)
+        repo_digest = _get_ppr_data_digest(ppr_data)
         # print(
         #     f'- - - hyperr - - -\n'
         #     f'{json.dumps(hyper_digest, indent=2)}\n'
         #     f'- - - repro- - -\n'
-        #     f'{json.dumps(repro_digest, indent=2)}\n'
+        #     f'{json.dumps(repo_digest, indent=2)}\n'
         # )
         # input()
 
         # note general vals
         hyper_repro_data.append(
             list(hyper_digest.values()) +
-            list(repro_digest.values())
+            list(repo_digest.values())
         )
         hyper_repro_keys = list(hyper_digest.keys()) \
-            + list(repro_digest.keys())
+            + list(repo_digest.keys())
 
-        if repro_digest['repo_stars'] < star_threshold:
+        if repo_digest['repo_stars'] < star_threshold:
+            continue
+        if ppr_data['github_metadata']['full_name'] in filter_repos:
+            continue
+
+        # use only papers that have a “Cite” or “Citation” section
+        # in their GitHub repo’s README
+        cite_sec_pattern = re.compile(
+            r'^(#+|\*\*)\s*cit[eia]',
+            re.I | re.M
+        )
+        if not cite_sec_pattern.search(ppr_data['github_metadata']['readme']):
             continue
 
         # note selected vals
-        repo_stars_vals.append(repro_digest['repo_stars'])
-        gt0log = lambda x: np.log2(x) if x > 0 else x  # noqa: E731
-        log_repo_stars_vals.append(gt0log(repro_digest['repo_watchers']))
-        repo_forks_vals.append(repro_digest['repo_forks'])
-        repo_open_issues_vals.append(repro_digest['repo_open_issues'])
-        n_vpa_vals.append(
+        log2save = lambda x: (  # noqa: E731
+            np.log2(x) if np.log2(x) not in [np.nan, np.inf, -np.inf] else 0
+        )
+        n_vpa_val = (
             hyper_digest['n_vpa'] +
             hyper_digest['n_vp'] +
             hyper_digest['n_pa']
         )
+        repo_corr_val = (
+            repo_digest['repo_closed_issues'] / repo_digest['repo_stars']
+            if repo_digest['repo_stars'] > 0 else 0
+        )
+        # if repo_digest['repo_closed_issues'] > 400:
+        #     print(
+        #         ppr_data['github_metadata']['full_name'],
+        #         repo_digest['repo_closed_issues']
+        #     )
+        # if (
+        #     log2save(repo_digest['repo_closed_issues']) > np.log(1) and
+        #     log2save(n_vpa_val) > np.log2(1)
+        # ):
+        repo_corr_vals.append(repo_corr_val)
+        n_vpa_vals.append(log2save(n_vpa_val))
+        # # # # #
+        log_repo_stars_vals.append(log2save(repo_digest['repo_watchers']))
+        repo_forks_vals.append(repo_digest['repo_forks'])
+        repo_open_issues_vals.append(repo_digest['repo_open_issues'])
 
     # print(len(hyper_data))
     # print(len(repro_data))
@@ -244,11 +291,11 @@ def analyse(ppr_preds, ppr_datas):
 
     print(f'---===[ star threshold: {star_threshold} ]===---')
 
-    # n_vpa <-> repo_stars
-    cc, p = pearsonr(n_vpa_vals, repo_stars_vals)
+    # n_vpa <-> repo_corr_vals
+    cc, p = pearsonr(n_vpa_vals, repo_corr_vals)
 
-    # n_vpa <-> repo_stars, rounded to 3 decimal places without leading 0
-    print(f'n_vpa <-> repo_stars')
+    # n_vpa <-> repo_corr_vals, rounded to 3 decimal places without leading 0
+    print(f'n_vpa <-> repo_corr_vals')
     print(f'{cc:.3f} ({p:.3f})')
 
     # print(
@@ -283,27 +330,28 @@ def analyse(ppr_preds, ppr_datas):
     #     f'p-value: {p}'
     # )
 
-    # scatter plot n_vpa_vals and repo_stars_vals
+    # scatter plot n_vpa_vals and repo_corr_vals (defined above)
     fig, ax = plt.subplots(figsize=(10, 10))
     ax.scatter(
-        n_vpa_vals, repo_stars_vals,
+        n_vpa_vals, repo_corr_vals,
         alpha=0.5
     )
     ax.set_xlabel('n_vpa')
-    ax.set_ylabel('repo_stars')
+    ax.set_ylabel('repo_corr_vals')
     plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('prediction_data_root_dir', type=str)
+    parser.add_argument('prediction_data_fp', type=str)
+    parser.add_argument('paper_metadata_fp', type=str)
     args = parser.parse_args()
 
     # load prediction data
-    ppr_preds = load_pediction_data(args.prediction_data_root_dir)
+    ppr_preds = load_pediction_data(args.prediction_data_fp)
 
     # load external paper metadata
-    with open(f'merged_data.json', 'r') as f:
+    with open(args.paper_metadata_fp, 'r') as f:
         ppr_data = json.load(f)
 
     analyse(ppr_preds, ppr_data)
